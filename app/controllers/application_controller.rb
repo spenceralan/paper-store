b4f6225e9930c87fd9f581df5001ab0713411882
Spencer Alan <spencer.alan@icloud.com>
2017-07-07 21:09:46 -0700

    add sitewide authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+protected
 
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
