ffce1767633b0dd39bd3eb030b45e7a18d90254c
Spencer Alan <spencer.alan@icloud.com>
2017-07-09 22:27:11 -0700

    add faker seed data

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :name, :description, presence: true
 
   validates :price,
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,39 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create([
-  {
-    username: "admin",
-    email: "admin@admin.com",
-    password: "admin123456",
-    password_confirmation: "admin123456",
-    admin: true
+require 'faker'
 
-  },
-  {
-    username: "testuser",
-    email: "test@test.com",
-    password: "123456",
-    password_confirmation: "123456"
-  },
-])
+Product.destroy_all
+
+admin = User.create(
+  username: "admin",
+  email: "admin@admin.com",
+  password: "admin123456",
+  password_confirmation: "admin123456",
+  admin: true
+)
+
+user = User.create(
+  username: "testuser",
+  email: "test@test.com",
+  password: "123456",
+  password_confirmation: "123456"
+)
+
+250.times do
+  number_of_words = Faker::Number.between(1, 5)
+  number_of_reviews = Faker::Number.between(5, 30)
+  rating = Faker::Number.between(1, 5)
+  product = Product.create!(
+    name: Faker::Hipster.words(number_of_words).join(" ").titleize,
+    description: Faker::DrWho.catch_phrase,
+    price: Faker::Number.decimal(2),
+  )
+  number_of_reviews.times do
+    product.reviews.create!(
+      user_id: user.id,
+      content: Faker::RuPaul.quote,
+      rating: rating,
+    )
+  end
+end
