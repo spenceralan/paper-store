d715408c0d4887245c254782fb6df22b4610f277
Spencer Alan <spencer.alan@icloud.com>
2017-07-09 21:58:12 -0700

    update validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
   has_many :reviews
+  validates :name, :description, presence: true
+
+  validates :price,
+    presence: true,
+    numericality: true
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  validates :content, presence: true
+  
+  validates :rating,
+    presence: true,
+    inclusion: { 
+      in: 1..5
+    }
 end
