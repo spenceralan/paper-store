e165a21f46f4973fc5a5bf389c61687e5bbcf1af
Spencer Alan <spencer.alan@icloud.com>
2017-07-10 01:33:11 -0700

    update model specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 describe Product do
 
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :price }
+
+  it { should have_many :reviews }
+
   it { should validate_numericality_of(:price) }
   it { should have_attached_file(:image) }
   it { should validate_attachment_content_type(:image).
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 describe Review do
+
+  it { should validate_presence_of :content }
+  it { should validate_presence_of :rating }
+
+  it { should belong_to :product }
+  it { should belong_to :user }
+
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
 end
